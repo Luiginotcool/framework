e030b98e647589a64d4b7e0edb213e1da3e280cb
Tom Carpenter <59624298+Luiginotcool@users.noreply.github.com>
2024-08-04 22:09:22 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,51 @@
+App = {}
+
+
+
+
+App.init = function() {
+    App.canvas = document.getElementById("canvas");
+    App.width = window.innerWidth;
+    App.height = window.innerHeight;
+    App.canvas.width = App.width;
+    App.canvas.height = App.height;
+    App.frames = 0;
+    App.oldTimeStamp = 0;
+
+    App.noLoop = false;
+
+    Graphics.context = App.canvas.getContext("2d");
+    Graphics.fg = "black"
+    Graphics.bg = "#776065"
+
+    App.testGraphics();
+
+
+    window.requestAnimationFrame(App.gameLoop);
+}
+
+App.gameLoop = function(timeStamp) {
+    if (App.noLoop) {
+        window.requestAnimationFrame(App.gameLoop);
+    } else {
+        App.dt = (timeStamp - App.oldTimeStamp);
+        App.oldTimeStamp = timeStamp;
+
+        let fps = Math.round(1000 / App.dt);
+
+        Graphics.drawFps(fps);
+        App.frames++;
+
+        App.noLoop = false;
+        window.requestAnimationFrame(App.gameLoop);
+    }
+}
+
+
+App.testGraphics = function() {
+    Graphics.background();
+    Graphics.drawCircle(100, 200, 20);
+}
+
+
+App.init();
---
+++ graphics.js
@@ -0,0 +1,63 @@
+Graphics = {}
+
+Graphics.drawArrow = function(startX, startY, endX, endY, arrowSize) {
+    var context = Graphics.context;
+    context.fillStyle = Graphics.fg
+    // Calculate arrow angle
+    var angle = Math.atan2(endY - startY, endX - startX);
+
+    // Draw line
+    context.beginPath();
+    context.moveTo(startX, startY);
+    context.lineTo(endX, endY);
+    context.stroke();
+
+    // Draw arrowhead
+    context.beginPath();
+    context.moveTo(endX, endY);
+    context.lineTo(endX - arrowSize * Math.cos(angle - Math.PI / 6), endY - arrowSize * Math.sin(angle - Math.PI / 6));
+    context.lineTo(endX - arrowSize * Math.cos(angle + Math.PI / 6), endY - arrowSize * Math.sin(angle + Math.PI / 6));
+    context.closePath();
+    context.fill();
+}
+
+Graphics.line = function(x1, y1, x2, y2, colour = Graphics.fg) {
+    let context = Graphics.context;
+    context.beginPath();
+    context.moveTo(x1, y1);
+    context.lineTo(x2,y2);
+    context.stroke();
+}
+
+Graphics.drawFps = function(fps) {
+    Graphics.context.fillStyle = "white";
+    //Graphics.context.fillRect(0,0,200,100);
+    Graphics.context.font = "15px Arial";
+    Graphics.context.fillText(`${fps} fps`, 10, 30);
+    Graphics.context.fillText(`Heading: ${Game.camera.theta}`, 80, 30)
+}
+
+Graphics.background = function(colour = Graphics.bg) {
+    Graphics.context.fillStyle = colour;
+    Graphics.context.fillRect(0, 0, App.canvas.width, App.canvas.height);
+}
+
+Graphics.fillRect = function(x, y, w, h, colour = Graphics.fg) {
+    Graphics.context.fillStyle = colour;
+    Graphics.context.fillRect(x, y, w, h)
+}
+
+Graphics.drawCircle = function(x, y, radius, fill, stroke, strokeWidth) {
+    let ctx = Graphics.context
+    ctx.beginPath()
+    ctx.arc(x, y, radius, 0, 2 * Math.PI, false)
+    if (fill) {
+      ctx.fillStyle = fill
+      ctx.fill()
+    }
+    if (stroke) {
+      ctx.lineWidth = strokeWidth
+      ctx.strokeStyle = stroke
+      ctx.stroke()
+    }
+}
